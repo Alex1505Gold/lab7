09ba1bc65e2e1769b3d02310635c3fb1037d2b2e
Alex1505Gold <49805582+Alex1505Gold@users.noreply.github.com>
2020-12-07 18:31:54 +0300

    Исправлена очередь

--- lab7.cpp
+++ lab7.cpp
@@ -20,15 +20,13 @@ void constructor_q(Queue_struct& q)
 
 void push_q(Queue_struct& q, Queue_element& n)
 {
-    
+    Queue_element* newQ_e = new Queue_element;
+    newQ_e->value = n.value;
     if (q.q_begin == nullptr) {
-        Queue_element* newQ_e = new Queue_element;
-        newQ_e->value = n.value;
         q.q_begin = newQ_e;
         q.q_end = newQ_e;
+        
     } else if (q.q_begin != nullptr) {
-        Queue_element* newQ_e = new Queue_element;
-        newQ_e->value = n.value;
         q.q_end->qe_next = newQ_e;
         q.q_end = newQ_e;
     }
@@ -39,10 +37,7 @@ Queue_element pop_q(Queue_struct& q)
 {
     int a = q.q_begin->value;
     Queue_element rez;
-    Queue_element* tmp = new Queue_element;
-    tmp = q.q_begin;
     q.q_begin = q.q_begin->qe_next;
-    delete tmp;
     rez.value = a;
     rez.qe_next = q.q_begin;
     return rez;
@@ -57,17 +52,34 @@ unsigned int size_q(Queue_struct& q)
         counter++;
         current = current->qe_next;
     }
+    delete current;
     return counter;
+}
+
+void print_q(Queue_struct& q)
+{
+    Queue_element* current = new Queue_element;
+    current = q.q_begin;
+    while (current != nullptr) {
+        std::cout << current->value << " ";
+        current = current->qe_next;
+    }
+    std::cout << std::endl;
+    delete current;
+}
+
+void del_elem(Queue_element* q)
+{
+    if (q != nullptr)
+    {
+        del_elem(q->qe_next);
+        delete q;
+    }
 }
 
 void destructor_q(Queue_struct& q)
 {
-    while (q.q_begin != nullptr) {
-        Queue_element* tmp = new Queue_element;
-        tmp = q.q_begin;
-        q.q_begin = q.q_begin->qe_next;
-        delete tmp;
-    }
+    del_elem(q.q_begin);
 }
 
 
@@ -82,6 +94,7 @@ int main()
         cur_element.value = a;
         push_q(queue_0, cur_element);
     }
+    print_q(queue_0);
     std::cout << "there is " << size_q(queue_0) << " in queue." << std::endl;
     cur_element = pop_q(queue_0);
     std::cout << "Get the first element from queue. It is " << cur_element.value << std::endl;
@@ -90,6 +103,7 @@ int main()
     cur_element = pop_q(queue_0);
     std::cout << "Get the first element from queue. It is " << cur_element.value << std::endl;
     std::cout << "there is " << size_q(queue_0) << " in queue." << std::endl;
+    print_q(queue_0);
     destructor_q(queue_0);
     return 0;
 }
