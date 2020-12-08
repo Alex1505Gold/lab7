ad76c997133f57f3d9e05b75d54a125031a58bb1
Alex1505Gold <49805582+Alex1505Gold@users.noreply.github.com>
2020-12-08 20:30:18 +0300

    Исправленный стек

--- lab7_stack.cpp
+++ lab7_stack.cpp
@@ -14,36 +14,28 @@ struct Stack_struct
 
 void constructor_st(Stack_struct& st)
 {
-    st.st_begin = nullptr;
     st.st_end = nullptr;
 }
 
 void push_st(Stack_struct& st, Stack_element& n)
 {
+    Stack_element* newSt_e = new Stack_element;
+    newSt_e->value = n.value;
     if (st.st_begin == nullptr) {
-        Stack_element* newSt_e = new Stack_element;
-        newSt_e->value = n.value;
         st.st_begin = newSt_e;
         st.st_end = newSt_e;
     }
     else if (st.st_begin != nullptr) {
-        Stack_element* newSt_e = new Stack_element;
-        newSt_e->value = n.value;
         newSt_e->ste_prev = st.st_end;
         st.st_end = newSt_e;
     }
-    
-
 }
 
 Stack_element pop_st(Stack_struct& st)
 {
     int a = st.st_end->value;
     Stack_element rez;
-    Stack_element* tmp = new Stack_element;
-    tmp = st.st_end;
     st.st_end = st.st_end->ste_prev;
-    delete tmp;
     rez.value = a;
     rez.ste_prev = st.st_end;
     return rez;
@@ -58,17 +50,37 @@ unsigned int size_st(Stack_struct& st)
         counter++;
         current = current->ste_prev;
     }
+    delete current;
     return counter;
+}
+
+void print_elem(Stack_element* st)
+{
+    if (st != nullptr)
+    {
+        print_elem(st->ste_prev);
+        std::cout << st->value << " ";
+    }
+}
+
+void print_st(Stack_struct& st)
+{
+    print_elem(st.st_end);
+    std::cout << std::endl;
+}
+
+void del_elem(Stack_element* st)
+{
+    if (st != nullptr)
+    {
+        del_elem(st->ste_prev);
+        delete st;
+    }
 }
 
 void destructor_st(Stack_struct& st)
 {
-    while (st.st_end != nullptr) {
-        Stack_element* tmp = new Stack_element;
-        tmp = st.st_end;
-        st.st_end = st.st_end->ste_prev;
-        delete tmp;
-    }
+    del_elem(st.st_end);
 }
 
 
@@ -83,14 +95,26 @@ int main()
         cur_element.value = a;
         push_st(stack_0, cur_element);
     }
+    print_st(stack_0);
     std::cout << "there is " << size_st(stack_0) << " in stack." << std::endl;
     cur_element = pop_st(stack_0);
     std::cout << "Get the last element from stack. It is " << cur_element.value << std::endl;
+    print_st(stack_0);
     cur_element = pop_st(stack_0);
     std::cout << "Get the last element from stack. It is " << cur_element.value << std::endl;
+    print_st(stack_0);
+    cur_element = pop_st(stack_0);
+    std::cout << "Get the last element from stack. It is " << cur_element.value << std::endl;
+    print_st(stack_0);
+    std::cout << "there is " << size_st(stack_0) << " in stack." << std::endl;
+    std::cin >> a;
+    cur_element.value = a;
+    push_st(stack_0, cur_element);
+    std::cout << "there is " << size_st(stack_0) << " in stack." << std::endl;
     cur_element = pop_st(stack_0);
     std::cout << "Get the last element from stack. It is " << cur_element.value << std::endl;
     std::cout << "there is " << size_st(stack_0) << " in stack." << std::endl;
+    print_st(stack_0);
     destructor_st(stack_0);
     return 0;
 }
